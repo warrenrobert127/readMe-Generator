55c2444f0f71c888dce34b075b7d6bfc42e05e81
Robert Warren <warrenrobert127@gmail.com>
2022-02-20 14:58:33 -0600

    add ?s

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,125 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is your name? (Required)",
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log("Please enter your name!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Enter your GitHub Username (Required)",
+    validate: (githubInput) => {
+      if (githubInput) {
+        return true;
+      } else {
+        console.log("Please enter your GitHub username!");
+        return false;
+      }
+    },
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is your Project Title? (Required)",
+      validate: (titleInput) => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log("Please enter your project title!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Enter your project description (Required)",
+      validate: (descriptionInput) => {
+        if (descriptionInput) {
+          return true;
+        } else {
+          console.log("Please enter your project description!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "installation",
+      message:
+        "What are the steps required to install your project?  (Required)",
+      validate: (installationInput) => {
+        if (installationInput) {
+          return true;
+        } else {
+          console.log("Please enter instructions for installing your project!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Provide instructions and examples for use (Required)",
+      validate: (descriptionInput) => {
+        if (descriptionInput) {
+          return true;
+        } else {
+          console.log("Please provide instructions and examples for use!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Provide instructions and examples for use (Required)",
+      validate: (usageInput) => {
+        if (usageInput) {
+          return true;
+        } else {
+          console.log("Please provide instructions and examples for use!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "confirm",
+      name: "confirmAbout",
+      message:
+        'Would you like to enter some information about yourself for an "About" section?',
+      default: true,
+    },
+    {
+      type: "input",
+      name: "about",
+      message: "Provide some information about yourself:",
+      when: ({ confirmAbout }) => confirmAbout,
+    },
+  ]);
+};
+
 
 // Function call to initialize app
 init();
